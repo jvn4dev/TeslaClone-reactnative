e7e3a9ee4860829ec9eaf36bbc2ec74df1cfb57e
정성준 <chung@jeongseongjun-ui-MacBookPro.local>
2021-04-30 13:13:55 +0900

    add 2 buttons

--- components/carItem/index.js
+++ components/carItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View, ImageBackground } from 'react-native';
+import StyledButton from '../styledButton';
 import styles from './styles';
 
 const CarItem = () => {
@@ -13,6 +14,20 @@ const CarItem = () => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+      <StyledButton
+        type="primary"
+        content="Custom Order"
+        onPress={() => {
+          console.warn('Custom Order was pressed');
+        }}
+      />
+      <StyledButton
+        type="secondary"
+        content="Existing Inventory"
+        onPress={() => {
+          console.warn('Existing Inventory was pressed');
+        }}
+      />
     </View>
   );
 };

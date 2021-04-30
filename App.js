03cc359f87b7fcf2c9f4a392df3ccd68937fb08d
정성준 <chung@jeongseongjun-ui-MacBookPro.local>
2021-04-30 17:55:26 +0900

    add FlatList and Header

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import CarItem from './components/carItem';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/carsList';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
-        name={'Model S'}
-        tagline={'Order Online for'}
-        taglineCTA={'Touchless Delivery'}
-        image={require('./assets/images/ModelS.jpeg')}
-      />
+      <Header />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
